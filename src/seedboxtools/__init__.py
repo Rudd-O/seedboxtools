7fc69aca6fe903de7539c7c43badcbf30ae3d925
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2012-03-02 01:15:31 -0800

    Completely reimplemented the Transmission Daemon side of the equation

--- src/seedboxtools/__init__.py
+++ src/seedboxtools/__init__.py
@@ -7,11 +7,22 @@ import os
 
 class SeedboxClient:
 
-    def __init__(self
-            local_download_dir):
+    def __init__(self, local_download_dir):
         self.local_download_dir = local_download_dir
 
-    def get_finished_torrents():
+    def get_finished_torrents(self):
+        raise NotImplementedError
+    
+    def get_file_name(self,torrentname):
+        raise NotImplementedError
+
+    def transfer(self,filename):
+        raise NotImplementedError
+
+    def exists_on_server(self,filename):
+        raise NotImplementedError
+    
+    def remove_remote_download(self,filename):
         raise NotImplementedError
 
 
@@ -21,8 +32,8 @@ class TorrentFluxClient(SeedboxClient):
             hostname,
             base_dir,
             incoming_dir,
-            fluxcli_path,
             torrentinfo_path,
+            fluxcli_path,
         ):
         SeedboxClient.__init__(self,local_download_dir)
         self.hostname = hostname
@@ -30,8 +41,9 @@ class TorrentFluxClient(SeedboxClient):
         self.incoming_dir = incoming_dir
         self.fluxcli_path = fluxcli_path
         self.torrentinfo_path = torrentinfo_path
-        self.getssh = partial(util.ssh_getstdout,hostname=hostname)
-        self.passthru = partial(util.ssh_passthru,hostname=hostname)
+        
+        self.getssh = partial(util.ssh_getstdout,hostname)
+        self.passthru = partial(util.ssh_passthru,hostname)
 
     def get_finished_torrents(self):
         stdout = self.getssh([self.fluxcli,"transfers"])
@@ -43,7 +55,7 @@ class TorrentFluxClient(SeedboxClient):
 
     def get_file_name(self,torrentname):
         fullpath = os.path.join(self.base_dir,".transfers",torrentname)
-        stdout = getssh(["LANG=C",self.torrentinfo_path,fullpath).splitlines()
+        stdout = getssh(["env","LANG=C",self.torrentinfo_path,fullpath]).splitlines()
         filenames = [ l[22:] for l in stdout if l.startswith("file name...........: ") ]
         if not len(filenames):
                 filelistheader = stdout.index("files...............:")
@@ -56,10 +68,10 @@ class TorrentFluxClient(SeedboxClient):
     def transfer(self,filename):
         # need to single-quote the *path* for the purposes of the remote shell so it doesn't fail, because the path is used in the remote shell
         path = os.path.join(self.incoming_dir,filename)
-        path = shell_quote(path)
+        path = util.shell_quote(path)
         path = "%s:%s"%(self.hostname,path)
         opts = ["-arvzP"]
-        cmdline = [ "rsync" ] + opts + [ path , local_download_dir ]
+        cmdline = [ "rsync" ] + opts + [ path , self.local_download_dir ]
         returncode = util.passthru(cmdline)
         return returncode
 
@@ -72,8 +84,100 @@ class TorrentFluxClient(SeedboxClient):
         elif returncode == -2: raise IOError(4,"exists_on_server interrupted")
         else: raise AssertionError, "exists on server returned %s"%returncode
 
-    def remove_remote_download(filename):
+    def remove_remote_download(self,filename):
         returncode = self.passthru(["rm","-rf",os.path.join(self.incoming_dir,filename)])
+        if returncode == 0: return
+        elif returncode == -2: raise IOError(4,"remove_remote_download interrupted")
+        else: raise AssertionError, "remove dirs only returned %s"%returncode
+
+
+class TransmissionClient(SeedboxClient):
+    def __init__(self,
+            local_download_dir,
+            hostname,
+            torrents_dir,
+            incoming_dir,
+            torrentinfo_path,
+            transmission_remote_path,
+            transmission_remote_user,
+            transmission_remote_password,
+        ):
+        SeedboxClient.__init__(self,local_download_dir)
+        self.hostname = hostname
+        self.torrents_dir = torrents_dir
+        self.incoming_dir = incoming_dir
+        self.transmission_remote_path = transmission_remote_path
+        self.transmission_remote_user = transmission_remote_user
+        self.transmission_remote_password = transmission_remote_password
+        
+        self.getssh = partial(util.ssh_getstdout,hostname)
+        self.passthru = partial(util.ssh_passthru,hostname)
+
+    def get_finished_torrents(self):
+        stdout = util.getstdout([
+            self.transmission_remote_path,
+            self.hostname,
+            "--auth=%s:%s"%(self.transmission_remote_user,self.transmission_remote_password),
+            "-l"
+        ])
+        stdout = stdout.splitlines()[1:-1]
+        stdout.reverse()
+        stdout = [ x.split() + [x[70:-1]] for x in stdout ]
+        stdout = [ (x[0],x[8],x[-1]) for x in stdout if x[8] in "Finished Seeding" ]
+        self.torrent_to_id_map = dict((x[2],x[0]) for x in stdout)
+        pairs = [ (x[2],x[1]) for x in stdout]
+        return pairs
+
+    def get_file_name(self,torrentname):
+        # first, cache the torrent names to IDs
+        if not hasattr(self,"torrent_to_id_map"): self.get_finished_torrents()
+        torrent_id = self.torrent_to_id_map[torrentname]
+        stdout = util.getstdout([
+            "env","LANG=C",
+            self.transmission_remote_path,
+            self.hostname,
+            "--auth=%s:%s"%(self.transmission_remote_user,self.transmission_remote_password),
+            "-t",torrent_id,
+            "-f"
+        ]).splitlines()
+        filename = util.firstcomponent(stdout[2][34:])
+        return filename
+
+    def transfer(self,filename):
+        # need to single-quote the *path* for the purposes of the remote shell so it doesn't fail, because the path is used in the remote shell
+        path = os.path.join(self.incoming_dir,filename)
+        path = util.shell_quote(path)
+        path = "%s:%s"%(self.hostname,path)
+        opts = ["-arvzP"]
+        cmdline = [ "rsync" ] + opts + [ path , self.local_download_dir ]
+        returncode = util.passthru(cmdline)
+        return returncode
+
+    def exists_on_server(self,filename):
+        path = os.path.join(self.incoming_dir,filename)
+        cmd = ["test","-e",path]
+        returncode = self.passthru(cmd)
+        if returncode == 1: return False
+        elif returncode == 0: return True
+        elif returncode == -2: raise IOError(4,"exists_on_server interrupted")
+        else: raise AssertionError, "exists on server returned %s"%returncode
+
+    def remove_remote_download(self,filename):
+        if not hasattr(self,"torrent_to_id_map"): self.get_finished_torrents()
+        if not hasattr(self,"filename_to_torrent_map"):
+            self.filename_to_torrent_map = dict(
+                (self.get_file_name(torrentname),torrentname) for torrentname,status in self.get_finished_torrents()
+            )
+        torrent = self.filename_to_torrent_map[torrent]
+        torrent_id = self.torrent_to_id_map[torrent]
+        returncode = util.passthru([
+            "env","LANG=C",
+            self.transmission_remote_path,
+            self.hostname,
+            "--auth=%s:%s"%(self.transmission_remote_user,self.transmission_remote_password),
+            "-t",torrent_id,
+            "--remove-and-delete"
+        ])
         if returncode == 0: return
         elif returncode == -2: raise IOError(4,"remove_remote_download interrupted")
         else: raise AssertionError, "remove dirs only returned %s"%returncode
--- src/seedboxtools/util.py
+++ src/seedboxtools/util.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from subprocess import Popen,PIPE,STDOUT,call
+import os
 
 def shell_quote(shellarg):
         return"'%s'"%shellarg.replace("'",r"'\''")
@@ -17,7 +18,8 @@ def getstdoutstderr(cmdline,inp=None): # return stoud and stderr in a single str
         if p.returncode != 0: raise Exception, "Command %s return code %s"%(cmdline,p.returncode)
         return output
 
-def passthru(cmdline): return call(cmdline) # return status code, pass the outputs thru
+def passthru(cmdline):
+    return call(cmdline) # return status code, pass the outputs thru
 
 def quote_cmdline(cmdline):
     """Quote a command line in list form for SSH usage"""
@@ -29,4 +31,14 @@ def ssh_getstdout(hostname,cmdline):
 
 def ssh_passthru(hostname,cmdline):
     cmd = quote_cmdline(cmdline)
-    return call(["ssh","-o","BatchMode yes","-o","ForwardX11 no",hostname,cmd] )
+    return passthru(["ssh","-o","BatchMode yes","-o","ForwardX11 no",hostname,cmd] )
+
+def firstcomponent(path):
+    if not path: raise ValueError, "path cannot be empty: %r"%path
+    oldpath = path
+    while True:
+        path = os.path.dirname(path)
+        if not path or os.path.dirname(path) == path: break
+        oldpath = path
+    return oldpath
+    
