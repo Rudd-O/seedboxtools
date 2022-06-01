ab0a8d873d56f70e19bc2f37bae7de98d5746475
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-06-01 03:13:03 +0000

    Fix test.

--- src/seedboxtools/test_util.py
+++ src/seedboxtools/test_util.py
@@ -2,5 +2,5 @@ import seedboxtools.util as m
 
 def test_which():
    # Should be true on most Unices.
-   assert m.which("true") == "/bin/true"
+   assert m.which("true").endswith("true")
    assert m.which("narostnaironstio") == None
